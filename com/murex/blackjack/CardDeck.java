74359d4219cce8d8fad8e54f91fafa4dfb9746d0
jackbyrnemurex <jackbyrne124@gmail.com>
2016-11-04 13:34:32 +0000

    Add files via upload

---
+++ com/murex/blackjack/CardDeck.java
@@ -0,0 +1,11 @@
+package com.murex.blackjack;
+
+public class CardDeck implements Deck {
+
+	@Override
+	public Card[] getCards() {
+		// TODO Auto-generated method stub
+		return new Card[52];
+	}
+
+}
