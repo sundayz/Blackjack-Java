3a626a0ea5174e7bf760ed881159eed3ebf08e38
mtek <mtek@ie.murex.com>
2016-11-04 15:34:26 +0000

    Merge branch.

--- com/murex/blackjack/TestGame.java
+++ com/murex/blackjack/TestGame.java
@@ -3,10 +3,12 @@ package com.murex.blackjack;
 public class TestGame {
 	
 	public static void main(String[] args){
+		
+		int PlayerScore;
+		int DealerScore;
+		
 		Deck deck = new CardDeck();
-		
 		Dealer dealer = new Dealer(deck);
-		
 		Player player = new Player();
 		
 		
@@ -16,16 +18,23 @@ public class TestGame {
 		player.addCard(dealer.dealCard());
 		dealer.addCard(dealer.dealCard());
 		
-		player.getHand().getValue();
-		dealer.getHand().getValue();
-		
 		for(Card card : player.getHand().getCards()){
-			System.out.println(card);
+			System.out.println("Player got " + card);
 		}
 		
 		for(Card card : dealer.getHand().getCards()){
-			System.out.println(card);
+			System.out.println("Dealer got " + card);
 		}
+		
+		PlayerScore = player.getHand().getValue();
+		DealerScore = dealer.getHand().getValue();
+		
+		if (PlayerScore > DealerScore) {
+			System.out.printf("Player wins %d points to %d.", PlayerScore, DealerScore);
+		} else {
+			System.out.printf("Dealer wins %d points to %d.", DealerScore, PlayerScore);
+		}
+
 		
 	}
 
